f30a7c27a6b590bf09a847dd091d563cfb583f22
Jan Kaspar <jan.kaspar@gmail.com>
2020-09-25 16:14:05 +0200

    filter on unprescaled HLT bits from the SingleMuon dataset

--- template_cfg.py
+++ template_cfg.py
@@ -1,6 +1,7 @@
 import sys 
 import os
 import FWCore.ParameterSet.Config as cms
+import copy
 
 process = cms.Process("PPSXangleBetaExtraction")
 
@@ -39,11 +40,27 @@ process.maxEvents = cms.untracked.PSet(
   input = cms.untracked.int32(-1)
 )
 
+# filter on unprescaled HLT bits from the SingleMuon dataset
+from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+process.hltFilter = copy.deepcopy(hltHighLevel)
+process.hltFilter.TriggerResultsTag = cms.InputTag("TriggerResults", "", "HLT")
+
+year="$year"
+
+if year == "2016":
+  process.hltFilter.HLTPaths = ['HLT_IsoMu24_v*']
+if year == "2017":
+  process.hltFilter.HLTPaths = ['HLT_IsoMu27_v*']
+if year == "2018":
+  process.hltFilter.HLTPaths = ['HLT_IsoMu24_v*']
+
 # configure plotter
 process.load("Validation.CTPPS.ctppsLHCInfoPlotter_cfi")
 process.ctppsLHCInfoPlotter.outputFile = "output.root"
 
 # processing sequence
 process.path = cms.Path(
-    process.ctppsLHCInfoPlotter
+    process.hltFilter
+
+    * process.ctppsLHCInfoPlotter
 )
