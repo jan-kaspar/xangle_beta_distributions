ea5d76dd708d75f6439647e82b3960e0c64b7321
Jan Kaspar <jan.kaspar@cern.ch>
2020-09-27 17:38:37 +0200

    Added post processing and 2021 scenario.

--- extract.cc
+++ extract.cc
@@ -12,9 +12,32 @@ TH2D* Process(const string &period, TH2D *input)
 {
 	TH2D *h = new TH2D(*input);
 
-	// TODO
+	// post-processing
+	if (period == "2017_postTS2")
+	{
+		for (int bix = 1; bix <= h->GetNbinsX(); ++bix)
+		{
+			for (int biy = 1; biy <= h->GetNbinsY(); ++biy)
+			{
+				//const double x = h->GetXaxis()->GetBinCenter(bix);
+				const double y = h->GetYaxis()->GetBinCenter(biy);
+
+				if (y > 0.30001)
+					h->SetBinContent(bix, biy, 0.);
+			}
+		}
+	}
 
 	return h;
+}
+
+//----------------------------------------------------------------------------------------------------
+
+TH2D* MakeSingleBinHistogram(double xangle, double betaStar)
+{
+    TH2D* h2 = new TH2D("", ";xangle;betaStar", 3, xangle-1.5, xangle+1.5, 3, betaStar-0.015, betaStar+0.015);
+    h2->Fill(xangle, betaStar);
+    return h2;
 }
 
 //----------------------------------------------------------------------------------------------------
@@ -44,11 +67,15 @@ int main()
 
 		gDirectory = f_out->mkdir(period.c_str());
 
-		h2->Write("h2_betaStar_vs_xangle_original");
+		//h2->Write("h2_betaStar_vs_xangle_original");
 		Process(period, h2)->Write("h2_betaStar_vs_xangle");
 
 		delete f_in;
 	}
+
+	// add extra scenarios
+	gDirectory = f_out->mkdir("2021");
+	MakeSingleBinHistogram(140., 0.30)->Write("h2_betaStar_vs_xangle");
 
 	delete f_out;
 
